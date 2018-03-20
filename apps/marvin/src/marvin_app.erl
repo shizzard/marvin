e6851806bcb637bb8c207661cfe1dcf0138f2237
Denis Fakhrtdinov <me@shizz.ru>
2018-03-20 19:13:00 +0300

    marvin_gateway_meta metrics

--- apps/marvin/src/marvin_app.erl
+++ apps/marvin/src/marvin_app.erl
@@ -11,7 +11,26 @@
 
 
 start(_StartType, _StartArgs) ->
+    declare_metrics(),
     marvin_sup:start_link().
 
 stop(_State) ->
     ok.
+
+
+declare_metrics() ->
+    application:ensure_started(prometheus),
+    %% marvin_gateway_meta
+    prometheus_histogram:new([
+        {name, marvin_gateway_meta_http_request_duration_us},
+        {help, "Gateway metadata request duration in microseconds"},
+        {buckets, prometheus_buckets:new({linear, 100000, 50000, 10})}
+    ]),
+    prometheus_gauge:new([
+        {name, marvin_gateway_meta_http_request_interval_s},
+        {help, "Gateway metadata requests interval in seconds"}
+    ]),
+    prometheus_gauge:new([
+        {name, marvin_gateway_meta_shards_count_i},
+        {help, "Amount of shards started"}
+    ]).
--- apps/marvin/src/marvin_gateway_meta.erl
+++ apps/marvin/src/marvin_gateway_meta.erl
@@ -7,7 +7,7 @@
 -record(state, {
     gun_pid :: pid() | undefined,
     gun_stream_reference :: reference() | undefined,
-    last_time_gather = 0 :: non_neg_integer(),
+    http_req_start_time = {0, 0, 0} :: erlang:timestamp(),
     next_gather_after = 0 :: non_neg_integer(),
     shards_count = 0 :: non_neg_integer(),
     running_shards_count = 0 :: non_neg_integer(),
@@ -44,6 +44,7 @@ start_link() ->
 
 
 init(_) ->
+    do_report_shards_count(0),
     {ok, on_start, #state{}, {state_timeout, 0, ?gun_connect()}}.
 
 
@@ -76,31 +77,33 @@ handle_event(state_timeout, ?gather_meta(), on_gun_up, S0) ->
         {<<"authorization">>, Authorization}
     ]),
     {next_state, on_gather_meta_headers, S0#state{
+        http_req_start_time = os:timestamp(),
         gun_stream_reference = StreamRef
     }};
 
 handle_event(info, ?gun_response(_ConnPid, StreamRef, nofin, 200, Data), on_gather_meta_headers, #state{
-    gun_stream_reference = StreamRef
+    gun_stream_reference = StreamRef,
+    http_req_start_time = HttpReqStartTime
 } = S0) when is_list(Data) ->
-    case proplists:get_value(<<"x-ratelimit-reset">>, Data, undefined) of
+    do_report_http_req_duration(HttpReqStartTime),
+    IntervalSec = case proplists:get_value(<<"x-ratelimit-reset">>, Data, undefined) of
         undefined ->
             marvin_log:error(
                 "Error while gathering meta: no 'x-ratelimit-reset' header found, falling to +10s",
                 []
             ),
-            {next_state, on_gather_meta_body, S0#state{
-                last_time_gather = marvin_helper_time:timestamp(),
-                next_gather_after = 10 * 1000
-            }};
+            10 * 1000;
         TimestampBin ->
             FutureTimestamp = binary_to_integer(TimestampBin),
             CurrentTimestamp = marvin_helper_time:timestamp(),
-            IntervalSec = (FutureTimestamp - CurrentTimestamp) * 2,
-            {next_state, on_gather_meta_body, S0#state{
-                last_time_gather = marvin_helper_time:timestamp(),
-                next_gather_after = IntervalSec * 1000
-            }}
-    end;
+            CalculatedIntervalSec = (FutureTimestamp - CurrentTimestamp) * 2,
+            true = (CalculatedIntervalSec > 0),
+            CalculatedIntervalSec
+    end,
+    do_report_http_request_interval(IntervalSec),
+    {next_state, on_gather_meta_body, S0#state{
+        next_gather_after = IntervalSec * 1000
+    }};
 
 handle_event(info, ?gun_data(_ConnPid, StreamRef, nofin, Data), on_gather_meta_body, #state{
     gun_stream_reference = StreamRef
@@ -136,6 +139,7 @@ handle_event(state_timeout, ?maybe_rebuild_layout(), on_maybe_rebuild_layout, #s
     ),
     ShardsToStop = lists:nthtail(ShardsCount, lists:seq(0, RunningShardsCount - 1)),
     lists:foreach(fun marvin_shard_sup:stop_shard_rx/1, ShardsToStop),
+    do_report_shards_count(ShardsCount),
     {next_state, on_gun_up, S0#state{
         running_shards_count = ShardsCount
     }, {state_timeout, S0#state.next_gather_after, ?gather_meta()}};
@@ -153,6 +157,7 @@ handle_event(state_timeout, ?maybe_rebuild_layout(), on_maybe_rebuild_layout, #s
     lists:foreach(fun(ShardId) ->
         marvin_shard_sup:start_shard_session(ShardId, WssUrl)
     end, ShardsToStart),
+    do_report_shards_count(ShardsCount),
     {next_state, on_gun_up, S0#state{
         running_shards_count = ShardsCount
     }, {state_timeout, S0#state.next_gather_after, ?gather_meta()}};
@@ -167,6 +172,40 @@ handle_event({call, From}, ?get_shards_count(), _AnyState, S0) ->
 
 
 %% Internals
+
+
+-spec do_report_http_req_duration(HttpReqStartTime :: erlang:timestamp()) ->
+    marvin_helper_type:ok_return().
+
+do_report_http_req_duration(HttpReqStartTime) ->
+    prometheus_histogram:observe(
+        marvin_gateway_meta_http_request_duration_us,
+        [],
+        timer:now_diff(os:timestamp(), HttpReqStartTime)
+    ),
+    ok.
+
+
+-spec do_report_http_request_interval(IntervalSec :: pos_integer()) ->
+    marvin_helper_type:ok_return().
+
+do_report_http_request_interval(IntervalSec) ->
+    prometheus_gauge:set(
+        marvin_gateway_meta_http_request_interval_s,
+        IntervalSec
+    ),
+    ok.
+
+
+-spec do_report_shards_count(ShardsCount :: pos_integer()) ->
+    marvin_helper_type:ok_return().
+
+do_report_shards_count(ShardsCount) ->
+    prometheus_gauge:set(
+        marvin_gateway_meta_shards_count_i,
+        ShardsCount
+    ),
+    ok.
 
 
 -spec get_gateway_url() ->
