8448bc7948249a31ca5c1782ffd65af704cbe98c
Denis Fakhrtdinov <me@shizz.ru>
2018-03-23 20:52:37 +0300

    marvin_pdu2_status_update

---
+++ apps/marvin_pdu2/src/marvin_pdu2_status_update.erl
@@ -0,0 +1,78 @@
+-module(marvin_pdu2_status_update).
+-compile({parse_transform, cloak_transform}).
+
+-export([export/1]).
+% since   ?integer    unix time (in milliseconds) of when the client went idle, or null if the client is not idle
+% game    ?activity object    null, or the user's new activity
+% status  string  the user's new status
+% afk bool    whether or not the client is afk
+-record(?MODULE, {
+    since = undefined :: since() | undefined,
+    game = undefined :: game() | undefined,
+    status :: status(),
+    afk = false :: afk()
+}).
+
+-define(status_online, <<"online">>).
+-define(status_offline, <<"offline">>).
+-define(status_idle, <<"idle">>).
+-define(status_dnd, <<"dnd">>).
+-define(status_invisible, <<"invisible">>).
+
+-type since() :: pos_integer().
+-type game() :: marvin_pdu2_object_game:t().
+-type status() :: unicode:unicode_binary().
+-type afk() :: boolean().
+-type t() :: #?MODULE{}.
+
+-export_type([since/0, game/0, status/0, afk/0, t/0]).
+
+
+%% Interface
+
+
+
+cloak_validate(since, null) ->
+    {ok, undefined};
+
+cloak_validate(since, Value) when is_integer(Value) andalso Value > 0 ->
+    {ok, Value};
+
+cloak_validate(game, null) ->
+    {ok, undefined};
+
+cloak_validate(game, Value) ->
+    {ok, marvin_pdu2_object_game:new(Value)};
+
+cloak_validate(status, Value)
+when is_binary(Value) andalso (
+    ?status_online == Value
+    orelse ?status_offline == Value
+    orelse ?status_idle == Value
+    orelse ?status_dnd == Value
+    orelse ?status_invisible == Value
+) ->
+    {ok, Value};
+
+cloak_validate(afk, Value) when is_boolean(Value) ->
+    {ok, Value};
+
+cloak_validate(_, _) ->
+    {error, invalid}.
+
+
+export(#?MODULE{
+    since = Since,
+    game = Game,
+    status = Status,
+    afk = Afk
+}) ->
+    #{
+        <<"since">> => marvin_pdu2:nullify(Since),
+        <<"game">> => case Game of
+            undefined -> null;
+            _ -> marvin_pdu2_object_game:export(Game)
+        end,
+        <<"status">> => Status,
+        <<"afk">> => Afk
+    }.
